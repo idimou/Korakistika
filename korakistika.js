01c418487d724e190452234db332daa1649b03ce
idimou <jdimou@gmail.com>
2019-05-17 18:59:21 +0200

    initial commit

---
+++ korakistika.js
@@ -0,0 +1,15 @@
+﻿function ShowHelloMessage()
+{
+    var name = document.getElementById("myname");
+    document.getElementById("hellomessage").innerHTML = "Hellooooo!!!, " + name.value; + name.value
+}
+
+document.getElementById("mybutton").onclick = ShowHelloMessage;
+
+
+
+function Translate() {
+    var inputText = document.getElementById('inputText').value;
+    var translatedText = "translated text";
+    return translatedText;
+}
