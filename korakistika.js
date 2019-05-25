9fa2b371b4479f3c7a2e2f06331b0ab4cf8d0168
idimou <jdimou@gmail.com>
2019-05-25 18:12:58 +0200

    from VS code

--- korakistika.js
+++ korakistika.js
@@ -1,8 +1,20 @@
 ﻿function ShowTranslatedText() {
+
+
     var inputText = document.getElementById('inputText').value;
+
+    var hyphenate = createHyphenator(hyphenationPatternsEnGb);
+
+    var hyphenatedText = hyphenate(inputText);
+
     var translatedText = "ka"+inputText;
-    document.getElementById("TranslatedTextBox").value = translatedText;
+    document.getElementById("TranslatedTextBox").value = hyphenatedText;
+
+
     return translatedText;
 }
 document.getElementById("TranslateButton").onclick = ShowTranslatedText;
+
+
+
 
